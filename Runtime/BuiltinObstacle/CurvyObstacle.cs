bef6661f93779ef6b14ef4718ef5700c15e568d7
kaiyumPC <kaiyumcg@gmail.com>
2022-12-19 21:16:39 +0600

    Update CurvyObstacle.cs

--- Runtime/BuiltinObstacle/CurvyObstacle.cs
+++ Runtime/BuiltinObstacle/CurvyObstacle.cs
@@ -6,64 +6,85 @@ using UnityExt;
 
 namespace Adrenaline
 {
+    [System.Serializable]
+    public class DelayPattern
+    {
+        [SerializeField] internal List<float> delayPattern;
+    }
+
+
     public class CurvyObstacle : GameObstacle
     {
         [SerializeField] List<PathCreator> paths;
         [SerializeField] float speed = 5f;
-        [SerializeField, Range(0.0f, 1.0f)] float startOffset = 0.1f;
         [SerializeField] Vector3 pathPositionOffset;
         [SerializeField] Transform holder;
         [SerializeField] List<GameObject> targets;
-        [SerializeField] List<float> delayPattern;
+        [SerializeField] List<DelayPattern> delayPatterns;
+        [SerializeField] bool isLooped = false;
+        [SerializeField] bool randomizedDelayPattern = false;
 
-        #region ForOverrideSupport
-        public List<PathCreator> Paths { get { return paths; } set { paths = value; } }
-        public float Speed { get { return speed; } set { speed = value; } }
-        public float StartOffset { get { return startOffset; } set { startOffset = value; } }
-        public Vector3 PathPositionOffset { get { return pathPositionOffset; } set { pathPositionOffset = value; } }
-        public List<float> DelayPattern { get { return delayPattern; } set { delayPattern = value; } }
-        #endregion
-        protected override void OnStartReceiverDeath(IObstacleReceiverActor receiver)
+        protected override void OnStartReceiverDeath(IObstacleReceiverActor reciever)
         {
-            base.OnStartReceiverDeath(receiver);
+            base.OnStartReceiverDeath(reciever);
             StopAllCoroutines();
             var anims = GetComponentsInChildren<Animator>(true);
             anims.ExForEachSafe((anim) =>
             {
-                if (anim != null)
-                {
-                    anim.cullingMode = AnimatorCullingMode.CullCompletely;
-                    anim.SetBool("moving", false);
-                    anim.SetFloat("speed", 0.0f);
-                }
+                anim.SetBool("moving", false);
+                anim.SetFloat("speed", 0.0f);
             });
         }
         protected override void OnStartObstacle()
         {
             base.OnStartObstacle();
             targets.ExForEachSafe((it) => { it.SetActive(false); });
-            paths.ExForEachSafe((path) => { StartCoroutine(Follower(path)); });
-            IEnumerator Follower(PathCreator path)
+            paths.ExForEachSafe((path, index) => { StartCoroutine(Follower(path, index)); });
+            IEnumerator Follower(PathCreator path, int index)
             {
                 var maxDist = path.path.length;
                 int id = 0;
-                while (true)
+                this.targets.ExShuffle();
+                var patterns = delayPatterns[index].delayPattern;
+                if (randomizedDelayPattern)
                 {
-                    var dt = Time.deltaTime;
-                    for (int i = 0; i < targets.Count; i++)
-                    {
-                        var t = targets[i];
-                        CloneAndStartMoving(t);
-                        float delay = 0.0f;
-
-                        if (id > delayPattern.Count - 1) { id = 0; }
-                        delay = delayPattern[id];
-                        yield return new WaitForSeconds(delay);
-                        id++;
-                    }
-                    yield return null;
+                    patterns.ExShuffle();
                 }
-                void CloneAndStartMoving(GameObject item)
+
+                var d = maxDist;
+                var startPatternIndex = 0;
+                var startTargetIndex = 0;
+                while (d > 0.0f)
+                {
+                    d -= speed * patterns[startPatternIndex];
+                    CloneAndStartMoving(targets[startTargetIndex], manualStartPosition: true, d);
+
+                    startPatternIndex++;
+                    if (startPatternIndex > patterns.Count - 1) { startPatternIndex = 0; }
+                    startTargetIndex++;
+                    if (startTargetIndex > targets.Count - 1) { startTargetIndex = 0; }
+                }
+
+                if (!isLooped)
+                {
+                    while (true)
+                    {
+                        var dt = Time.deltaTime;
+                        for (int i = 0; i < targets.Count; i++)
+                        {
+                            float delay = 0.0f;
+                            if (id > patterns.Count - 1) { id = 0; }
+                            delay = patterns[id];
+                            yield return new WaitForSeconds(delay);
+                            id++;
+
+                            var t = targets[i];
+                            CloneAndStartMoving(t);
+                        }
+                        yield return null;
+                    }
+                }
+                void CloneAndStartMoving(GameObject item, bool manualStartPosition = false, float manualDistance = 0.0f)
                 {
                     var orit = item.transform;
                     var obj = Instantiate(item) as GameObject;
@@ -74,7 +95,7 @@ namespace Adrenaline
                     t.localEulerAngles = orit.localEulerAngles;
                     t.localScale = orit.localScale;
                     var maxDist = path.path.length;
-                    float dist = startOffset * maxDist;
+                    float dist = manualStartPosition ? manualDistance : 0.0f;
 
                     var anims = obj.GetComponentsInChildren<Animator>(true);
                     anims.ExForEachSafe((anim) =>
@@ -89,18 +110,34 @@ namespace Adrenaline
                     {
                         while (true)
                         {
-                            dist += speed * Time.smoothDeltaTime;
+                            dist += speed * Time.deltaTime;
                             if (dist > maxDist)
                             {
-                                Destroy(obj);
-                                break;
+                                if (isLooped)
+                                {
+                                    var delta = dist - maxDist;
+                                    dist = delta;
+                                }
+                                else
+                                {
+                                    Destroy(obj);
+                                    break;
+                                }
                             }
-                            
+
                             var posFromPathCreator = path.path.GetPointAtDistance(dist);
                             Vector3 tg_pos = posFromPathCreator + pathPositionOffset;
                             Quaternion tg_rot = t.rotation;
                             var p1 = path.path.GetPointAtDistance(dist);
                             var p2 = path.path.GetPointAtDistance(dist + 0.1f);
+
+                            var secondDist = dist + 0.1f;
+                            if (secondDist > maxDist)
+                            {
+                                p1 = path.path.GetPointAtDistance(maxDist - 0.1f);
+                                p2 = path.path.GetPointAtDistance(maxDist);
+                            }
+
                             var lookDir = p2 - p1;
                             if (lookDir.magnitude > 0.02f)
                             {
@@ -108,7 +145,9 @@ namespace Adrenaline
                                 normDir = normDir.magnitude < 0.001f ? Vector3.up : normDir;
                                 tg_rot = Quaternion.LookRotation(lookDir, normDir);
                             }
-                            t.SetPositionAndRotation(tg_pos, tg_rot);
+                            t.position = tg_pos;
+                            t.rotation = Quaternion.Slerp(t.rotation, tg_rot, 12f * Time.deltaTime);
+                            //t.SetPositionAndRotation(tg_pos, tg_rot);
                             yield return null;
                         }
                     }
